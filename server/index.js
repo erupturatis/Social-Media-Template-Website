92347f2ee07d4fc1e4eb6d9996d3e8148c35c06f
erupturatis <bb.octav@gmail.com>
2022-11-15 11:14:36 +0200

    finished posting

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,17 +14,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-let CONNECTION_URL =
-  "mongodb+srv://erupturatis:<password>@cluster0.wudiukh.mongodb.net/?retryWrites=true&w=majority".replace(
-    "<password>",
-    "erupturatis123"
-  );
-
-console.log(CONNECTION_URL);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
